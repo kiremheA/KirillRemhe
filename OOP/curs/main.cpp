cf7e009b0c28fce37ab5888514f5b9b9e8144efc
Kirill <KirillRemhe>
2024-10-30 19:56:00 +0700

    Изменена скорость выполения полного оборота планет вокруг солнца

--- OOP/curs/main.cpp
+++ OOP/curs/main.cpp
@@ -3,7 +3,6 @@
 #include <cmath>
 #include <iostream>
 
-// Абстрактный класс SpaceObject
 class SpaceObject {
 protected:
     double size;
@@ -16,21 +15,17 @@ public:
 
     virtual ~SpaceObject() {}
 
-    virtual void updatePosition(double time) = 0; // Чисто виртуальная функция
+    virtual void updatePosition(double time) = 0;
 
     virtual void draw(sf::RenderWindow& window) const {
         sf::CircleShape shape(size);
         shape.setFillColor(color);
-        shape.setOrigin(size, size); // Центрируем объект
+        shape.setOrigin(size, size);
         shape.setPosition(positionX, positionY);
         window.draw(shape);
     }
-
-    double getPositionX() const { return positionX; }
-    double getPositionY() const { return positionY; }
 };
 
-// Класс Planet, наследник SpaceObject
 class Planet : public SpaceObject {
 private:
     double orbitSpeed;
@@ -51,21 +46,17 @@ public:
     }
 };
 
-// Класс Star, наследник SpaceObject
 class Star : public SpaceObject {
 public:
     Star(double size, const sf::Color& color, double posX, double posY)
         : SpaceObject(size, color, posX, posY) {}
 
-    void updatePosition(double time) override {
-        // Звезда остается на месте, не двигается
-    }
+    void updatePosition(double time) override {}
 };
 
-// Класс SolarSystem для управления объектами
 class SolarSystem {
 private:
-    std::vector<SpaceObject*> objects; // Массив указателей на космические объекты
+    std::vector<SpaceObject*> objects;
 
 public:
     ~SolarSystem() {
@@ -91,29 +82,26 @@ public:
     }
 };
 
-// Основная программа
 int main() {
-    // Создание окна
-    sf::RenderWindow window(sf::VideoMode(1000, 800), "Solar System Simulation");
+    sf::RenderWindow window(sf::VideoMode(1000, 800), "Realistic Solar System Simulation");
 
     SolarSystem solarSystem;
 
-    // Центр орбит
     const double centerX = 500.0;
     const double centerY = 400.0;
 
-    // Создаем Солнце и все планеты
-    Star* sun = new Star(30.0, sf::Color::Yellow, centerX, centerY);
-    Planet* mercury = new Planet(4.0, sf::Color(169, 169, 169), centerX + 40.0, centerY, 0.04, 40.0, 35.0, centerX, centerY);
-    Planet* venus = new Planet(6.0, sf::Color(255, 223, 0), centerX + 70.0, centerY, 0.015, 70.0, 60.0, centerX, centerY);
-    Planet* earth = new Planet(8.0, sf::Color::Blue, centerX + 100.0, centerY, 0.01, 100.0, 90.0, centerX, centerY);
-    Planet* mars = new Planet(6.0, sf::Color::Red, centerX + 150.0, centerY, 0.008, 150.0, 130.0, centerX, centerY);
-    Planet* jupiter = new Planet(12.0, sf::Color(255, 165, 0), centerX + 200.0, centerY, 0.005, 200.0, 180.0, centerX, centerY);
-    Planet* saturn = new Planet(10.0, sf::Color(210, 180, 140), centerX + 250.0, centerY, 0.003, 250.0, 220.0, centerX, centerY);
-    Planet* uranus = new Planet(8.0, sf::Color(135, 206, 235), centerX + 300.0, centerY, 0.002, 300.0, 270.0, centerX, centerY);
-    Planet* neptune = new Planet(8.0, sf::Color(0, 0, 139), centerX + 350.0, centerY, 0.0015, 350.0, 320.0, centerX, centerY);
+    double timeScale = 1.0 / 86400.0;
 
-    // Добавляем объекты в солнечную систему
+    Star* sun = new Star(30.0, sf::Color::Yellow, centerX, centerY);
+    Planet* mercury = new Planet(4.0, sf::Color(169, 169, 169), centerX + 40.0, centerY, 2 * M_PI / 88, 40.0, 35.0, centerX, centerY);
+    Planet* venus = new Planet(6.0, sf::Color(255, 223, 0), centerX + 70.0, centerY, 2 * M_PI / 225, 70.0, 60.0, centerX, centerY);
+    Planet* earth = new Planet(8.0, sf::Color::Blue, centerX + 100.0, centerY, 2 * M_PI / 365, 100.0, 90.0, centerX, centerY);
+    Planet* mars = new Planet(6.0, sf::Color::Red, centerX + 150.0, centerY, 2 * M_PI / 687, 150.0, 130.0, centerX, centerY);
+    Planet* jupiter = new Planet(12.0, sf::Color(255, 165, 0), centerX + 200.0, centerY, 2 * M_PI / (12 * 365), 200.0, 180.0, centerX, centerY);
+    Planet* saturn = new Planet(10.0, sf::Color(210, 180, 140), centerX + 250.0, centerY, 2 * M_PI / (29 * 365), 250.0, 220.0, centerX, centerY);
+    Planet* uranus = new Planet(8.0, sf::Color(135, 206, 235), centerX + 300.0, centerY, 2 * M_PI / (84 * 365), 300.0, 270.0, centerX, centerY);
+    Planet* neptune = new Planet(8.0, sf::Color(0, 0, 139), centerX + 350.0, centerY, 2 * M_PI / (165 * 365), 350.0, 320.0, centerX, centerY);
+
     solarSystem.addObject(sun);
     solarSystem.addObject(mercury);
     solarSystem.addObject(venus);
@@ -124,28 +112,35 @@ int main() {
     solarSystem.addObject(uranus);
     solarSystem.addObject(neptune);
 
-    double time = 0.0;
+    sf::Clock clock;
+    double totalTime = 0.0;  
 
-    // Главный цикл программы
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Up) {
+                    timeScale *= 2.0; 
+                }
+                if (event.key.code == sf::Keyboard::Down) {
+                    timeScale /= 2.0; 
+                    if (timeScale < 1.0 / 86400.0) {
+                        timeScale = 1.0 / 86400.0;
+                    }
+                }
+            }
         }
 
-        // Обновляем позиции планет
-        time += 0.01;
-        solarSystem.update(time);
+        double elapsedSeconds = clock.restart().asSeconds();
+        totalTime += elapsedSeconds * timeScale;
 
-        // Очищаем окно
+        solarSystem.update(totalTime);
+
         window.clear(sf::Color::Black);
-
-        // Рисуем объекты
         solarSystem.draw(window);
-
-        // Отображаем окно
         window.display();
     }
 
